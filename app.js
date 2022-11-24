5aadb0ca0a480a6d01d379916fc601ca2f37c4cb
kishore <Kishorebalu123>
2022-11-24 12:59:06 +0000

    twitter

--- app.js
+++ app.js
@@ -179,8 +179,7 @@ app.get("/tweets/:tweetId/",authenticateToken, async(request,response)=>{
     SELECT * FROM follower INNER JOIN user ON user.user_id=follower.following_user_id
       WHERE follower.follower_user_id='${userDetails.user_id}' `
     const following = await database.all(followingQuery)
-   //console.log(following)
-  //console.log(tweet.user_id)
+   
   if(following.some((each)=>each.following_user_id===tweet.user_id)){
 
 const getTweetsQuery=`
